50af634b7271a631918fc87b5adb49475be8ff8f
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-01 21:20:53 -0400

    added Cell Phone class and varibles for getting and setting

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -2,14 +2,12 @@ package com.pluralsight;
 
 public class CellPhone {
 
-    int serialNumber = 12345678;
-
-    String Model = "iPhone 13 Pro";
-
-    String carrier = "AT&T";
-
-    String phoneNumber = "704-124-4567";
-
-    String owner = "Hignacio Virgen";
+    //Instance Varibles
+    int serialNumber = 12321321;
+    String Model = "";
+    String carrier = "";
+    String phoneNumber = "";
+    String owner = "";
 
 }
+
--- src/main/java/com/pluralsight/CellPhoneApp.java
+++ src/main/java/com/pluralsight/CellPhoneApp.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class CellPhoneApp {
+
+    public static void main(String[]args){
+
+        CellPhone myCellPhone = new CellPhone();
+
+        System.out.print("What is the serial number? ");
+        System.out.print("What model is the phone?");
+        System.out.println("Who is the carrier?");
+        System.out.println("What is the phone number?");
+        System.out.println("Who is the owner of the phone?");
+        
+    }
+
+
 }
