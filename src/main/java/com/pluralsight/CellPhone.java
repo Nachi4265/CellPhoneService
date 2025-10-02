2d73da9e6485a3f77485a5adad35779d6acd7883
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-01 20:25:09 -0400

    first commit

---
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class CellPhone {
+
+    int serialNumber = 12345678;
+
+    String Model = "iPhone 13 Pro";
+
+    String carrier = "AT&T";
+
+    String phoneNumber = "704-124-4567";
+
+    String owner = "Hignacio Virgen";
+
+}
